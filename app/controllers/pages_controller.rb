23b288320bab72c103f2ab8a9295d8d07ab805d0
Alef Ojeda de Oliveira <nemubatubag@gmail.com>
2018-11-26 00:10:17 -0200

    configuração das páginas estáticas

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,12 @@
 class PagesController < ApplicationController
   def index
   end
+  def sobre    
+  end
+  def cultura    
+  end
+  def mexilhao    
+  end
+  def contato    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   root to: 'pages#index'
+
+  get '/sobre', to: 'pages#sobre', as: 'sobre'
+  get '/cultura', to: 'pages#cultura', as: 'cultura'
+  get '/mexilhao', to: 'pages#mexilhao', as: 'mexilhao'
+  get '/contato', to: 'pages#contato', as: 'contato'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
